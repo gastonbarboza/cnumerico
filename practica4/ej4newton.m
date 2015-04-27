0c4f64195e9f0d8d7a35dc6625fa417388929f2f
gastonbarboza <barbozagaston@gmail.com>
2015-04-27 15:41:14 -0300

    Agrega contador

--- practica4/ej4newton.m
+++ practica4/ej4newton.m
@@ -2,17 +2,19 @@ function raiz = newton(f,g,a)
 	M=5000;
 	tol=0.00001;
 	x = a;
-	for k = 1:M
-		ev = f(x)
-		h = ev/g(x)
+	for k = 0:M
+		ev = f(x);
+		h = ev/g(x);
+		paso=k;
 		if abs(ev)<tol
-			raiz = x
+			raiz = x;
 			break
 		elseif abs(h)<tol
-			raiz = x
+			raiz = x;
 			break
 		else
-			x = x - h
+			x = x - h;
 		endif
 	endfor
+	paso
 endfunction
