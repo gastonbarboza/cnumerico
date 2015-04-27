1721096571e2835d6c64b4ebf177a133faa0a230
gastonbarboza <barbozagaston@gmail.com>
2015-04-27 15:40:44 -0300

    Optimiza y agrega contador
    Quita recursión y condición redundante (if c == 9 elseif abs(c) < tol else ...)

--- practica4/ej2regfalsi.m
+++ practica4/ej2regfalsi.m
@@ -1,22 +1,23 @@
-function raiz = regfalsi (f,a,b,tol)
+function raiz = regfalsi (f,a,b)
+	M=5000;
+	tol=0.00001;
 	if sign(f(a)) == sign(f(b))
 		printf("No se pueden hallar raíces (sign(a) == sign(b))  \n")
 		break
 	else
-		c = (a*f(b)-b*f(a))/(f(b)-f(a))
-		f(c)
-		if f(c) == 0
-			raiz = c;
-		else
-			if abs(f(c)) > tol
-				if sign(f(c)) == sign(f(a))
-					regfalsi (f,c,b,tol)
-				else
-					regfalsi (f,a,c,tol)
-				endif
-			else
+		for k=0:M
+			c = (a*f(b)-b*f(a))/(f(b)-f(a));
+			paso=k;
+			if abs(f(c)) < tol
 				raiz = c;
+				break
+			elseif sign(f(c)) == sign(f(a))
+				a=c;
+			else
+				b=c;
 			endif
-		endif
+		endfor
+		raiz = c;
+		paso
 	endif
 endfunction
